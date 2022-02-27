914624b39410aaf53e993b9ad8893a9cdc9c6eea
WellingtonFidelis <wellingtonf20@gmail.com>
2022-02-27 18:23:49 -0300

    feat(frontend): setting flash error messages.

--- web/auth.py
+++ web/auth.py
@@ -1,13 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
 # @auth.route("hello")
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 
 @auth.route("/logout")
@@ -15,6 +15,23 @@ def logout():
     return "<p>Log out</p>"
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    return "<p>Sign up</p>"
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        if len(email) < 4:
+            flash("E-mail must be greater than 4 characters.", category="error")
+        elif len(firstName) < 2:
+            flash("First name must be greater than 1 characters.", category="error")
+        elif password1 != password2:
+            flash("Passowrds don\`t match.", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters.", category="error")
+        else:
+            # add user to database
+            flash("Account created!", category="success")
+    return render_template("sign_up.html")
